312bfbf5ef3cf0605b78de3b9e52c223fc8670de
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2024-08-03 17:47:40 +0330

    Update 1151. Minimum Swaps to Group All 1's Together.py

--- 1151. Minimum Swaps to Group All 1's Together.py
+++ 1151. Minimum Swaps to Group All 1's Together.py
@@ -1,4 +1,10 @@
-# integrate with the local version
-class solution:
-  def min_swaps (self, data : list [int] ) -> int:
-    L, N = len (
+class solution :
+    def minSwaps (self, data : list [int]) -> int :
+        L, N = len (data), sum (data)
+        cur = min_Swaps = data [:N].count (0)
+        
+        for i in range (N, L):
+            cur += data [i - N]
+            cur -= data [i]
+            min_Swaps = min (min_Swaps, cur)
+        return min_Swaps
