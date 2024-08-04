18434030a612204a14df69c3e81b63890d50c7aa
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2024-08-04 11:35:37 +0330

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -34,7 +34,45 @@ class Solution:
         sorted_t = sorted(t)
         return sorted_s == sorted_t
 
-# 
+# Approach 2: Hash Table
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        
+        # Count the frequency of characters in string s
+        for x in s:
+            count[x] += 1
+        
+        # Decrement the frequency of characters in string t
+        for x in t:
+            count[x] -= 1
+        
+        # Check if any character has non-zero frequency
+        for val in count.values():
+            if val != 0:
+                return False
+        
+        return True
+
+# Approach 3: Hash Table (Using Array)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = [0] * 26
+        
+        # Count the frequency of characters in string s
+        for x in s:
+            count[ord(x) - ord('a')] += 1
+        
+        # Decrement the frequency of characters in string t
+        for x in t:
+            count[ord(x) - ord('a')] -= 1
+        
+        # Check if any character has non-zero frequency
+        for val in count:
+            if val != 0:
+                return False
+        
+        return True
 
 #+---------------------------|
 # Lessons
