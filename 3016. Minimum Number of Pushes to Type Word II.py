de73643f0c36d05e34bbedfeab2322b9d1c51844
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2024-08-06 08:42:13 +0330

    Update 3016. Minimum Number of Pushes to Type Word II.py

--- 3016. Minimum Number of Pushes to Type Word II.py
+++ 3016. Minimum Number of Pushes to Type Word II.py
@@ -9,3 +9,15 @@
 #+---------------------------|
 # Lessons
 #+---------------------------|
+
+# ord ():
+	# The ord() function in Python 3 is used to return the Unicode code point for a given character. 
+	# Essentially, it converts a character into its corresponding integer representation.
+'''
+Here's a simple example:
+# Get the Unicode code point for the character 'A'
+unicode_value = ord('A')
+print(unicode_value)  # Output: 65
+
+In this example, ord('A') returns 65, which is the Unicode code point for the character 'A'
+'''
