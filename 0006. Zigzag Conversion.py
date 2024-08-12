3bdd36b27e1cb3e556fe3e3a3b542b82463c8577
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2024-08-12 18:50:11 +0330

    Update 0006. Zigzag Conversion.py

--- 0006. Zigzag Conversion.py
+++ 0006. Zigzag Conversion.py
@@ -38,3 +38,28 @@ Constraints:
 s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 '''
+#+---------------------------|
+# Solutions
+#+---------------------------|
+'''
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        idx, d = 0, 1
+        rows = [[] for _ in range(numRows)]
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+
+        return ''.join(rows)
+'''
